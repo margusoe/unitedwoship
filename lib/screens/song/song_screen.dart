fbda76dbbe2cba003bef685109ea01b319f32f95
margusoe <margushighmore@gmail.com>
2025-11-03 17:08:29 +0800

    song screen worked

--- lib/screens/song/song_screen.dart
+++ lib/screens/song/song_screen.dart
@@ -13,6 +13,157 @@ class SongScreen extends StatefulWidget {
   State<SongScreen> createState() => _SongScreenState();
 }
 
+class SongLyrics extends StatelessWidget {
+  final String xmlContent;
+
+  const SongLyrics({super.key, required this.xmlContent});
+
+  @override
+  Widget build(BuildContext context) {
+    try {
+      final fixedXml =
+          '<root>${xmlContent.replaceAll('<br />', '<br/>').replaceAll('<br>', '<br/>')}</root>';
+      final document = XmlDocument.parse(fixedXml);
+      final root = document.rootElement;
+
+      List<Widget> songWidgets = [];
+      final sections = root.children
+          .whereType<XmlElement>()
+          .where((e) => e.name.local != 'br' && e.name.local != 'chord')
+          .toList();
+
+      if (sections.isNotEmpty) {
+        // Has sections like <verse>, <chorus>
+        for (var element in sections) {
+          songWidgets.add(
+            Padding(
+              padding: const EdgeInsets.only(top: 24.0, bottom: 8.0),
+              child: Text(
+                _formatSectionTitle(element.name.local),
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+
+          songWidgets.addAll(_parseNodes(element.nodes, context));
+        }
+      } else {
+        // No sections, just a list of nodes
+        songWidgets.addAll(_parseNodes(root.nodes, context));
+      }
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: songWidgets,
+      );
+    } catch (e) {
+      return Text(xmlContent);
+    }
+  }
+
+  List<Widget> _parseNodes(Iterable<XmlNode> nodes, BuildContext context) {
+    List<Widget> widgets = [];
+    var currentLineNodes = <XmlNode>[];
+    for (var node in nodes) {
+      if (node is XmlElement && node.name.local == 'br') {
+        if (currentLineNodes.isNotEmpty) {
+          widgets.add(_buildLine(currentLineNodes, context));
+        }
+        currentLineNodes = [];
+      } else {
+        currentLineNodes.add(node);
+      }
+    }
+    if (currentLineNodes.isNotEmpty) {
+      widgets.add(_buildLine(currentLineNodes, context));
+    }
+    return widgets;
+  }
+
+  Widget _buildLine(List<XmlNode> nodes, BuildContext context) {
+    var widgets = <Widget>[];
+    for (var i = 0; i < nodes.length; i++) {
+      final node = nodes[i];
+      if (node is XmlElement && node.name.local == 'chord') {
+        String chord = node.text;
+        String lyric = '';
+
+        if (i + 1 < nodes.length && nodes[i + 1] is XmlText) {
+          lyric = nodes[i + 1].text;
+          i++;
+        }
+
+        widgets.add(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                chord,
+                style: TextStyle(
+                  color: AppTheme.darkPrimaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                lyric,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        );
+      } else if (node is XmlText) {
+        widgets.add(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                node.text,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    }
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Wrap(
+        spacing: 8,
+        runSpacing: 8,
+        children: widgets,
+      ),
+    );
+  }
+
+  String _formatSectionTitle(String title) {
+    final regex = RegExp(r'([a-zA-Z]+)(\d*)');
+    final match = regex.firstMatch(title);
+    if (match != null) {
+      String text = match.group(1)!;
+      String number = match.group(2)!;
+      text = text[0].toUpperCase() + text.substring(1);
+      if (number.isNotEmpty) {
+        return '[$text $number]';
+      }
+      return '[$text]';
+    }
+    return '[$title]';
+  }
+}
+
 class _SongScreenState extends State<SongScreen> {
   double _scrollSpeed = 50;
   late String _currentKey;
@@ -56,7 +207,8 @@ class _SongScreenState extends State<SongScreen> {
         children: [
           Expanded(
             child: SingleChildScrollView(
-                padding: const EdgeInsets.all(16.0), child: Text(song.songxml)),
+                padding: const EdgeInsets.all(16.0),
+                child: SongLyrics(xmlContent: song.songxml)),
           ),
           _buildBottomControls(),
         ],
